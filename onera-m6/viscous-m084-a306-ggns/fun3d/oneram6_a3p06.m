82b4ff01a183c5d06988403025fcd71473b96c9c
Mike Park <Mike.Park@NASA.gov>
2017-10-23 09:58:11 -0700

    rescaled

---
+++ onera-m6/viscous-m084-a306-ggns/fun3d/oneram6_a3p06.m
@@ -0,0 +1,9 @@
+#!/usr/bin/env octave
+
+format long
+
+root = 810.491484086
+area_reference  = 1.15315084119231*root*root
+x_moment_length = 0.801672958512342*root
+y_moment_length = 1.47601797621980*root
+reynolds_number = 14.6e6/root
