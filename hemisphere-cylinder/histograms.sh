6bc169bd05bb3b33bd646c90f06bf22e4cac4203
Mike Park <Mike.Park@NASA.Gov>
2017-10-26 13:01:23 -0400

    run histograms

---
+++ hemisphere-cylinder/histograms.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -x
+
+../scripts/ref_histogram.sh ./sa-m06-a00-fun3d/hsc03.meshb ./sa-m06-a00-fun3d/hsc03-metric.solb
+../scripts/ref_histogram.sh ./pragmatic/hsc03_out.meshb ./pragmatic/hsc10_out.solb
+../scripts/ref_histogram.sh ./sa-m06-a00-fun3d/omega_h/hsc03.meshb ./sa-m06-a00-fun3d/omega_h/hsc03-metric.solb
+../scripts/ref_histogram.sh ./sa-m06-a00-fun3d/refine/refine-hsc03-metric.solb
+
+../scripts/ref_histogram.sh ./sa-m06-a00-fun3d/hsc10.meshb ./sa-m06-a00-fun3d/hsc10-metric.solb
+../scripts/ref_histogram.sh ./pragmatic/hsc10_out.meshb ./pragmatic/hsc10_out.solb
+../scripts/ref_histogram.sh ./sa-m06-a00-fun3d/refine/refine-hsc10.meshb ./sa-m06-a00-fun3d/hsc10-metric.solb
