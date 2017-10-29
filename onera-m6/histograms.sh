09ca619cecf44592d6c44c3653cfff5a0ebcc826
Mike Park <Mike.Park@NASA.gov>
2017-10-29 12:10:50 -0400

    new om6 hist for prag

--- onera-m6/histograms.sh
+++ onera-m6/histograms.sh
@@ -11,9 +11,11 @@ set -x
 ../scripts/ref_histogram.sh ./sa-m084-a306-fun3d/onera03.meshb ./sa-m084-a306-fun3d/onera03-metric.solb
 ../scripts/ref_histogram.sh ./sa-m084-a306-fun3d/refine/refine-onera03.meshb ./sa-m084-a306-fun3d/refine/refine-onera03-metric.solb
 ../scripts/ref_histogram.sh ./pragmatic/onera03_out.meshb ./pragmatic/onera03_out.solb
+../scripts/ref_histogram.sh ./pragmatic/onera03_out_new.meshb ./pragmatic/onera03_out_new.solb
 
 ../scripts/ref_histogram.sh ./sa-m084-a306-fun3d/onera10.meshb ./sa-m084-a306-fun3d/onera10-metric.solb
 ../scripts/ref_histogram.sh ./sa-m084-a306-fun3d/refine/refine-onera10.meshb ./sa-m084-a306-fun3d/refine/refine-onera10-metric.solb
 ../scripts/ref_histogram.sh ./pragmatic/onera10_out.meshb ./pragmatic/onera10_out.solb
+../scripts/ref_histogram.sh ./pragmatic/onera10_out_new.meshb ./pragmatic/onera10_out_new.solb
 
 
