26bc40c030f6e5427d49ca6a8eaa9e4bf3b0447b
Mike Park <Mike.Park@NASA.gov>
2017-12-22 05:33:22 -0500

    now reads solb directly

--- onera-m6/sa-m084-a306-fun3d/refine-run.sh
+++ onera-m6/sa-m084-a306-fun3d/refine-run.sh
@@ -17,7 +17,7 @@ transmesh ../${rootin}-metric.solb ${rootin}-metric.sol
 nohup ~/refine/egads/src/ref_driver \
     -i ../${rootin}.meshb \
     -g ~/cases/ugawg-solution-adapt-cases/onera-m6/geometry/onera-m6-sharp-te.egads \
-    -m ${rootin}-metric.sol \
+    -m ${rootin}-metric.solb \
     -o ${rootout} \
     < /dev/null > ${rootout}-stdout &
 
