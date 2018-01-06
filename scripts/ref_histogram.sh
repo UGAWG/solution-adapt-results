cb0d1438f9f5463cfd471cc641dc2bf8c971cd81
Mike Park <Mike.Park@NASA.Gov>
2018-01-06 11:35:52 -0500

    transmesh no longer needed, refine reads -metric.solb directly

--- scripts/ref_histogram.sh
+++ scripts/ref_histogram.sh
@@ -7,16 +7,10 @@ set -x
 meshb=$1
 solb=$2
 
-sol=${solb/.solb/.sol}
-
 ratio=${meshb/.meshb/-ratio.tec}
 qual=${meshb/.meshb/-qual.tec}
 
-transmesh ${solb} ${sol}
-
-ref_histogram_test ${meshb} ${sol}
-
-rm ${sol}
+ref_histogram_test ${meshb} ${solb}
 
 mv ref_histogram_ratio.tec ${ratio}
 mv ref_histogram_quality.tec ${qual}
